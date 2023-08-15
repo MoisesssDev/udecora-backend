68efc494ccae994cb42feceb2f5270259282330d
[Moises Almeida] <[moisesssprg@gmail.com]>
2023-08-14 22:37:07 -0300

    swagger ui

--- src/main/java/com/spring/backend/BackendApplication.java
+++ src/main/java/com/spring/backend/BackendApplication.java
@@ -3,7 +3,11 @@ package com.spring.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API UDECORA", version = "1", description = "API para gerenciar usuários, produtos e categorias"))
 public class BackendApplication {
 
 	public static void main(String[] args) {
